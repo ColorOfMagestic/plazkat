3794d2c3a57c47bced2e6c72e1569336a82dff05
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2022-04-20 10:00:17 +0500

    body scroll при активном меню

--- src/js/main/section/header.js
+++ src/js/main/section/header.js
@@ -1,13 +1,19 @@
 function mobileMenu() {
-  const menuToggle = document.querySelector(".header__menu-toggle");
+  const body = document.querySelector("body");
   const navMobile = document.querySelector(".header_nav--mobile");
+
+  const menuToggle = document.querySelector(".header__menu-toggle");
   const close = document.querySelector(".header_nav--mobile__header-close");
 
   menuToggle.addEventListener("click", () => {
     navMobile.classList.add("active");
+    if (navMobile.classList.contains("active")) {
+      body.style.overflow = "hidden";
+    }
   });
   close.addEventListener("click", () => {
     navMobile.classList.remove("active");
+    body.style.overflow = "scroll";
   });
 }
 
